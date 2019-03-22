4899df068df9735dd825ce8cdb0d9631b3c61c90
David Wu <dwu97@yahoo.com>
2019-03-22 13:31:47 -0400

    Disable about navbar

--- mysite/chat/routing.py
+++ mysite/chat/routing.py
@@ -5,5 +5,5 @@ from django.urls import re_path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'^ws/(?P<room_name>[^/]+)/$', ChatConsumer),
+    re_path(r'^/(?P<room_name>[^/]+)/$', ChatConsumer),
 ]
